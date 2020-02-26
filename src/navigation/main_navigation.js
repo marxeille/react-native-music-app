1535ced75f209c6ba37f33e0e9e552834b7cff22
ducdn <duc.dn@agiletech.vn>
2020-02-26 17:16:50 +0700

    [DEV] Config Bottom Tab + Android Color

--- src/navigation/main_navigation.js
+++ src/navigation/main_navigation.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView
+} from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -10,12 +17,62 @@ import PlayerComponent from '../ui/player/player_component';
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+const stylesIcons = StyleSheet.create({
+  stretch: {
+    width: 24,
+    height: 24,
+    resizeMode: 'stretch'
+  }
+});
+
+
 function getBottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="home" component={HomeComponent} />
-      <Tab.Screen name="search" component={SearchComponent} />
-      <Tab.Screen name="library" component={LibraryComponent} />
+    <Tab.Navigator tabBarOptions={{
+      activeBackgroundColor: '#4b3562',
+      inactiveBackgroundColor: '#4b3562',
+      inactiveTintColor: '#835db8',
+      activeTintColor: '#FFF'
+    }}>
+      <Tab.Screen
+        name="home"
+        component={HomeComponent}
+        options={{
+          tabBarLabel: 'Trang chủ',
+          tabBarIcon: (props) => (
+            <Image
+              style={[stylesIcons.stretch, { tintColor: props.color }]}
+              source={require('../assets/icons/tabs/logo.png')}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="search"
+        component={SearchComponent}
+        options={{
+          tabBarLabel: 'Tìm kiếm',
+          tabBarIcon: (props) => (
+            <Image
+              style={[stylesIcons.stretch, { tintColor: props.color }]}
+              source={require('../assets/icons/tabs/ic_search.png')}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="library"
+        component={LibraryComponent}
+        options={{
+          tabBarLabel: 'Thư viện',
+          tabBarIcon: (props) => (
+            <Image
+              style={[stylesIcons.stretch, { tintColor: props.color }]}
+              source={require('../assets/icons/tabs/ic_library.png')}
+            />
+          )
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -23,9 +80,16 @@ function getBottomTabNavigator() {
 
 export default function mainContainer() {
   return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name="bottomtab" component={getBottomTabNavigator} />
-      <Stack.Screen name='player' component={PlayerComponent} />
-    </Stack.Navigator>
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: '#4b3562',
+    }}>
+      <Stack.Navigator
+        headerMode='none'
+      >
+        <Stack.Screen name="bottomtab" component={getBottomTabNavigator} />
+        <Stack.Screen name='player' component={PlayerComponent} />
+      </Stack.Navigator>
+    </SafeAreaView>
   );
 }
