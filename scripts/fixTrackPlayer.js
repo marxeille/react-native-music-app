79fb2df5355e866cadc363a229611c1fb3ac1bbd
Son Lai <son.lha@agiletech.vn>
2020-03-23 15:54:01 +0700

    fix rn checkbox

--- scripts/fixTrackPlayer.js
+++ scripts/fixTrackPlayer.js
@@ -24,6 +24,12 @@ function replaceHookD() {
   );
 }
 
+function replaceRNCheckox() {
+  const content = fs.readFileSync('./scripts/indexRNCheckbox.js').toString();
+  fs.writeFileSync('./node_modules/react-native-check-box/index.js', content);
+}
+
 replaceAudioSwift();
 replaceHookD();
 replaceHook();
+replaceRNCheckox();
--- src/ui/player/player_component.js
+++ src/ui/player/player_component.js
@@ -9,18 +9,13 @@ import {
 } from 'react-native';
 import Images from '../../assets/icons/icons';
 import { RootContext, rootStore } from '../../data/context/root_context';
-import { RootStore } from '../../data/repository/root_store';
 import { observer } from 'mobx-react';
-import PlayerStore, { PlayerState } from '../../data/repository/player_store';
-import { trace } from 'mobx';
 import GestureRecognizer, {
   swipeDirections,
 } from 'react-native-swipe-gestures';
-import { skipToNext, skipToPrevious } from './service/play_service';
 import { wrap } from '../../themes';
 import { navigate } from '../../navigation/navigation_service';
 import SeekBar from './new_player/SeekBar';
-import TrackPlayer from 'react-native-track-player';
 
 @observer
 @wrap
@@ -38,36 +33,36 @@ export default class PlayerComponent extends Component {
     console.log('you swipe down', gestureState);
   }
 
-  onSwipeLeft(gestureState) {
-    console.log('you swipe left', gestureState);
-    skipToPrevious();
+  onSwipeLeft() {
+    if (
+      rootStore.playerStore?.trackIndex <
+      rootStore.playerStore?.getQueueSize() - 1
+    ) {
+      rootStore.playerStore?.changeSong('next');
+    }
   }
 
-  onSwipeRight(gestureState) {
-    console.log('you swipe right', gestureState);
-    skipToNext();
+  onSwipeRight() {
+    if (rootStore.playerStore?.trackIndex > 0) {
+      rootStore.playerStore?.changeSong('back');
+    }
   }
 
   onSwipe(gestureName, gestureState) {
     const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
     switch (gestureName) {
       case SWIPE_UP:
-        console.log('you SWIPE_UP right', gestureState);
         break;
       case SWIPE_DOWN:
-        console.log('you SWIPE_DOWN right', gestureState);
         break;
       case SWIPE_LEFT:
-        console.log('you SWIPE_LEFT right', gestureState);
         break;
       case SWIPE_RIGHT:
-        console.log('you SWIPE_RIGHT right', gestureState);
         break;
     }
   }
 
   render() {
-    console.log('DEBUG => player_component render');
     const config = {
       velocityThreshold: 0.3,
       directionalOffsetThreshold: 80,
@@ -84,7 +79,6 @@ export default class PlayerComponent extends Component {
             config={config}>
             <TouchableHighlight onPress={() => navigate('player')}>
               <View style={styles.container}>
-                {/* <View cls="bg-white heightFn-2"></View> */}
                 <SeekBar
                   slider={false}
                   trackLength={rootStore?.playerStore?.duration}
