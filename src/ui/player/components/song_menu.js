90847c8bb9a26389a8e85d302a14e14b4d97c3df
Son Lai <son.lha@agiletech.vn>
2020-05-05 09:45:45 +0700

    option artist done

--- src/ui/player/components/song_menu.js
+++ src/ui/player/components/song_menu.js
@@ -103,7 +103,7 @@ export default class SongMenu extends Component {
             </Text>
           </View>
         </View>
-        <View cls="pa3">
+        <View cls="pa3 heightFn-250">
           <ScrollView showsVerticalScrollIndicator={false}>
             <ActionItem
               onPress={() => this.addPlaylist(true)}
